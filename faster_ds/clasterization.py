aef30b1bc4d3a5990ca313c51e3e29a1dfa0c17e
Karine <63256029+KarGrigoryan@users.noreply.github.com>
2020-06-14 00:21:53 +0400

    Update clasterization.py
    test

--- faster_ds/clasterization.py
+++ faster_ds/clasterization.py
@@ -4,14 +4,16 @@ from sklearn import cluster
 
 
 
-class CusterInterface:
-    def method1(self, path: str, file_name: str) -> str:
-         """method description"""
-        pass
+class ClusterInterface:
+        @staticmethod
+        def method1(self, path: str, file_name: str) -> str:
+            """method description"""
+            pass
 
-    def method2(self, full_file_name: str) -> dict:
-        """method description"""
-        pass
+        @staticmethod
+        def method2(self, full_file_name: str) -> dict:
+            """method description"""
+            pass
 
 
 
@@ -19,34 +21,37 @@ class CusterInterface:
 
 
 class cluster_model:
-  @staticmethod
-  def data_normalization(X):
-    """
-    X_norm ---> dataframe
-    
-    """
-    X_Norm = preprocessing.normalize(X)
-  return X_Norm
+        @staticmethod
+        def data_normalization(X):
+            """
+            X_norm ---> dataframe
+            """
+            X_Norm = preprocessing.normalize(X)
+            return X_Norm
 
-  @staticmethod
-  def km_labels(X_norm, n_clusters=5):
-    """
-    X_norm---> normed dataframe
-    """
-    km2 = cluster.KMeans(n_clusters=n_clusters,init='random').fit(X_Norm)
-    labels = km2.labels_
-    labels = np.unique(labels)
-    return labels
-  @staticmethod
-  def km_centers(X_norm, n_clusters=5):
-     """
-    X_norm---> normed dataframe
-    """
-      km2 = cluster.KMeans(n_clusters=n_clusters,init='random').fit(X_Norm)
-      
-      return km2.cluster_centers_
-   def plot_km(X_norm, labels):
-    #labels=km2.labels_
-    
-    plt.scatter(X_Norm[:,0],X_Norm[:,2], c=labels, cmap='rainbow')
-    plt.show()
+        
+        @staticmethod
+        def km_labels(X_norm, n_clusters=5):
+            """
+            X_norm---> normed dataframe
+            """
+            km2 = cluster.KMeans(n_clusters=n_clusters,init='random').fit(X_Norm)
+            labels = km2.labels_
+            labels = np.unique(labels)
+            return labels
+        
+        
+        @staticmethod
+        def km_centers(X_norm, n_clusters=5):
+            """
+            X_norm---> normed dataframe
+            """
+            km2 = cluster.KMeans(n_clusters=n_clusters,init='random').fit(X_Norm)
+            return km2.cluster_centers_
+        
+        
+        @staticmethod
+        def plot_km(X_norm, labels):
+            #labels=km2.labels_
+            plt.scatter(X_Norm[:,0],X_Norm[:,2], c=labels, cmap='rainbow')
+            plt.show()
